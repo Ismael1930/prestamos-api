b1096106ded37b7f422505d3e4841c5e54684119
Ismael. <tu@email.com>
2025-11-28 23:07:26 -0400

    fix

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -7,7 +7,7 @@ export const getDatabaseConfig = (configService: ConfigService): TypeOrmModuleOp
   port: configService.get('DB_PORT'),
   username: configService.get('DB_USERNAME'),
   password: configService.get('DB_PASSWORD'),
-  database: configService.get('DB_DATABASE'),
+  database: configService.get('DB_NAME'),
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
   logging: true,
